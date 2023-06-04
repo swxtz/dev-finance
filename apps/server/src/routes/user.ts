e1fd6827722c537a885f41869f2fa29b1067ec68
Gustavo Mendonça <alf4r6@gmail.com>
2023-06-04 10:38:35 -0300

    server: user schema validate

--- apps/server/src/routes/user.ts
+++ apps/server/src/routes/user.ts
@@ -1,7 +1,45 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
 
 export async function userRoutes(app: FastifyInstance) {
-    app.get("/users", (req, reply) => {
-      return reply.send("Ok");
+  app.get("/users", (req, reply) => {
+    return reply.send("Ok");
+  });
+
+  app.post("/user", async (req, reply) => {
+    const schema = z.object({
+      name: z.string().min(3, { message: "O nome deve ter no minimo 3 caracteres" }),
+      email: z.string().email({ message: "E-mail não valido" }).min(5, { message: "o E-mail deve ter no minimo 5 caracteres" }),
+      password: z.string().min(6, { message: "A senha deve ter no minimo 6 caracteres" }),
     });
+
+    try {
+      const { name, email, password } = schema.parse(req.body);
+
+      const emailExist = await prisma.user.findFirst({
+        where: {
+          email
+        }
+      });
+
+      if(emailExist) {
+        return reply.code(400).send({ message: "Esse E-mail já foi utilizado" });
+      }
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          password,
+        },
+      });
+
+      return reply.code(201).send({ message: "Conta criada" });
+    } catch (err) {
+      if (err instanceof z.ZodError) {
+        return reply.code(400).send({ message: err.issues[0].message });
+      }
+    }
+  });
 }
