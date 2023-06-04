b087254105f43c75d5e430673654a4c7c51db57e
Gustavo Mendonça <alf4r6@gmail.com>
2023-06-04 11:12:16 -0300

    server: using bcrypt in password hash

--- apps/server/src/routes/user.ts
+++ apps/server/src/routes/user.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
+import bcrypt from "bcrypt";
 
 export async function userRoutes(app: FastifyInstance) {
   app.get("/users", (req, reply) => {
@@ -9,9 +10,16 @@ export async function userRoutes(app: FastifyInstance) {
 
   app.post("/user", async (req, reply) => {
     const schema = z.object({
-      name: z.string().min(3, { message: "O nome deve ter no minimo 3 caracteres" }),
-      email: z.string().email({ message: "E-mail não valido" }).min(5, { message: "o E-mail deve ter no minimo 5 caracteres" }),
-      password: z.string().min(6, { message: "A senha deve ter no minimo 6 caracteres" }),
+      name: z
+        .string()
+        .min(3, { message: "O nome deve ter no minimo 3 caracteres" }),
+      email: z
+        .string()
+        .email({ message: "E-mail não valido" })
+        .min(5, { message: "o E-mail deve ter no minimo 5 caracteres" }),
+      password: z
+        .string()
+        .min(6, { message: "A senha deve ter no minimo 6 caracteres" }),
     });
 
     try {
@@ -19,20 +27,28 @@ export async function userRoutes(app: FastifyInstance) {
 
       const emailExist = await prisma.user.findFirst({
         where: {
-          email
-        }
+          email,
+        },
       });
 
-      if(emailExist) {
-        return reply.code(400).send({ message: "Esse E-mail já foi utilizado" });
+      if (emailExist) {
+        return reply
+          .code(400)
+          .send({ message: "Esse E-mail já foi utilizado" });
       }
 
-      await prisma.user.create({
-        data: {
-          name,
-          email,
-          password,
-        },
+      const salts = Number(process.env.SALT_ROUNDS);
+
+      await bcrypt.genSalt(salts, async function (err, salt) {
+        bcrypt.hash(password, salt, async function (err, hash) {
+          await prisma.user.create({
+            data: {
+              name,
+              email,
+              password: hash,
+            },
+          });
+        });
       });
 
       return reply.code(201).send({ message: "Conta criada" });
