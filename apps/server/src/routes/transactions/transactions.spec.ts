d56a8b62c4460348c466d13328a22ba47f7aa9cc
Gustavo Mendonca <alf4r6@gmail.com>
2023-07-24 12:11:39 -0300

    test: it should not be possible to create a transaction without value

--- apps/server/src/routes/transactions/transactions.spec.ts
+++ apps/server/src/routes/transactions/transactions.spec.ts
@@ -48,4 +48,27 @@ describe("POST /transactions", async () => {
     expect(response.status).toBe(400);
     expect(response.body).toEqual({ message: "Required" });
   });
+
+  it("it should not be possible to create a transaction without value", async () => {
+    const transaction = {
+      title: "title",
+      description: "description",
+      date: "2021-01-01",
+      type: "EXPENSE",
+    };
+
+    const jonh = {
+      email: "jonh.doe@example.com",
+      password: "superPassword",
+    };
+
+    const token = await request(app.server).post("/auth/user").send(jonh);
+
+    const response = await request(app.server)
+      .post("/transactions")
+      .send(transaction)
+      .set("Authorization", `Bearer ${token.body.token}`);
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({ message: "Required" });
+  });
 });
