517a90a5ea00b31e463daf3951d09ca27e9f57d9
Gustavo Mendonca <alf4r6@gmail.com>
2023-07-24 12:10:24 -0300

    test: it should not be possible to create a transaction without description

--- apps/server/src/routes/transactions/transactions.spec.ts
+++ apps/server/src/routes/transactions/transactions.spec.ts
@@ -3,25 +3,49 @@ import request from "supertest";
 import { app } from "../../server";
 
 describe("POST /transactions", async () => {
-  
-  
   it("it should not be possible to create a transaction without title", async () => {
     const transaction = {
-      "description": "description",
-      "value": 100,
-      "date": "2021-01-01",
-      "type": "EXPENSE"
+      description: "description",
+      value: 100,
+      date: "2021-01-01",
+      type: "EXPENSE",
     };
 
     const jonh = {
-      "email": "jonh.doe@example.com",
-      "password": "superPassword"
+      email: "jonh.doe@example.com",
+      password: "superPassword",
     };
-  
+
     const token = await request(app.server).post("/auth/user").send(jonh);
 
-    const response = await request(app.server).post("/transactions").send(transaction).set("Authorization", `Bearer ${token.body.token}`);
+    const response = await request(app.server)
+      .post("/transactions")
+      .send(transaction)
+      .set("Authorization", `Bearer ${token.body.token}`);
     expect(response.status).toBe(400);
-    expect(response.body).toEqual({ "message": "Required" });
+    expect(response.body).toEqual({ message: "Required" });
+  });
+
+  it("it should not be possible to create a transaction without description", async () => {
+    const transaction = {
+      title: "title",
+      value: 100,
+      date: "2021-01-01",
+      type: "EXPENSE",
+    };
+
+    const jonh = {
+      email: "jonh.doe@example.com",
+      password: "superPassword",
+    };
+
+    const token = await request(app.server).post("/auth/user").send(jonh);
+
+    const response = await request(app.server)
+      .post("/transactions")
+      .send(transaction)
+      .set("Authorization", `Bearer ${token.body.token}`);
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({ message: "Required" });
   });
 });
