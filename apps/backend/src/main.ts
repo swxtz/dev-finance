b54ab31b246d781694cc382e03864540da9bd484
Gustavo Mendonça <alf4r6@gmail.com>
2023-09-29 14:26:59 -0300

    feat: dynamic ports

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    const port = process.env.HOSTPORT || 3000;
+    await app.listen(port);
+    console.log(
+        `Application is running on: ${await app.getUrl()} or http://localhost:${port}`,
+    );
 }
 bootstrap();
