49db45f45c62783ada8d68333f04be72ffca4a7f
Gustavo Mendonça <alf4r6@gmail.com>
2023-10-20 03:43:17 -0300

    feat: change auth rest method get to post

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, UsePipes } from "@nestjs/common";
+import { Body, Controller, Post, UsePipes } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { SignInDto } from "./dtos/signin-dtos";
 import { ZodValidationPipe } from "nestjs-zod";
@@ -9,7 +9,7 @@ import { ApiTags } from "@nestjs/swagger";
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
-    @Get()
+    @Post()
     @UsePipes(new ZodValidationPipe(SignInDto))
     async signIn(@Body() data: SignInDto) {
         return this.authService.SignIn(data);
