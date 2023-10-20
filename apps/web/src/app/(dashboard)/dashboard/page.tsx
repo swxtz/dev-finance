730ef2d9fc7b7a8553d39523176c5f5cfbf6e766
Gustavo Mendonça <alf4r6@gmail.com>
2023-10-20 05:06:08 -0300

    feat: fetch balance

--- apps/web/src/app/(dashboard)/dashboard/page.tsx
+++ apps/web/src/app/(dashboard)/dashboard/page.tsx
@@ -1,9 +1,33 @@
+
 // import { Logout } from "@/components/Dashboard/Button/Button";
 
 import { MoneyWrapper } from "@/components/Dashboard/MoneyWrapper/MoneyWrapper";
 import { NewTransactionModal } from "@/components/Dashboard/NewTransactionModal/NewTransactionModal";
+import { getToken } from "next-auth/jwt";
 
-export default function Dashboard() {
+
+const apiUrl = process.env.API_URL || "http://localhost:3333";
+
+async function getBalance() {
+    const token = await getToken({ raw: false });
+
+    if (!token) {
+        return;
+    }
+    console.log(token);
+    const response = await fetch(`${apiUrl}/users/balance`, {
+        headers: {
+            "Authorization": `Bearer ${localStorage.getItem(token.sub || "")}`
+        }
+    });
+    const data1 = await response.json();
+    console.log(data1);
+}
+
+export default async function Dashboard() {
+    const balance = await getBalance();
+
+    console.log(balance);
     return (
         <main className="mx-32">
             <div className="mt-20">
