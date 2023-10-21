9db0dd8417f89fc82dd7a4eab937cb91701ae867
Gustavo Mendonça <alf4r6@gmail.com>
2023-10-21 06:11:26 -0300

    fix: api token import type

--- apps/web/src/app/api/token/route.ts
+++ apps/web/src/app/api/token/route.ts
@@ -1,8 +1,7 @@
-import { NextApiRequest } from "next";
 import { getToken } from "next-auth/jwt";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextApiRequest) {
+export async function GET(req: NextRequest) {
     const secret = process.env.NEXTAUTH_SECRET;
 
     const token = await getToken({ req, secret });
