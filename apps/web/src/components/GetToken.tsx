a00accf2327758e8b03e81cd1f3ac5f034f0ef01
Gustavo Mendonça <alf4r6@gmail.com>
2023-10-21 05:59:23 -0300

    fix: eslint import rule

--- apps/web/src/components/GetToken.tsx
+++ apps/web/src/components/GetToken.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { signOut, useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
 
 export function GetToken() {
     // async function getToken() {
