cae6a44f49f349b19ca2e730bb2280dc25108b75
Gustavo Mendonça <alf4r6@gmail.com>
2023-11-18 20:12:53 -0300

    feat: token

--- apps/web/src/app/(dashboard)/dashboard/page.tsx
+++ apps/web/src/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,10 @@
-// import { Logout } from "@/components/Dashboard/Button/Button";
+
 "use client";
 import { MoneyWrapper } from "@/components/Dashboard/MoneyWrapper/MoneyWrapper";
+import { NewTransactionModal } from "@/components/Dashboard/NewTransactionModal/NewTransactionModal";
+import { signOut } from "next-auth/react";
 import { useEffect, useState } from "react";
+// import { signOut } from "next-auth/react";
 
 // async function getBalance() {
 //     // const token =
@@ -21,42 +24,65 @@ import { useEffect, useState } from "react";
 // }
 
 export default function Dashboard() {
-    const [data, setData] = useState(null);
+    const [token, setToken] = useState(null);
+    const [balance, setBalance] = useState(null);
+
+    const apiUrl = process.env.API_URL || "http://localhost:3333";
 
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchToken = async () => {
             try {
                 const response = await fetch("http://localhost:3000/api/token"); // Replace with your API endpoint
-                const jsonData = await response.json();
-                setData(jsonData);
+                const jsonToken = await response.json();
+                await console.log(jsonToken.token);
+                setToken(jsonToken);
             } catch (error) {
                 console.error("Error fetching data:", error);
             }
         };
 
-        fetchData();
+        fetchToken();
     }, []);
+
+    useEffect(() => {
+        const fetchBalance = async () => {
+            try {
+                const tokenToParse = `Bearer ${token}`;
+                const response = await fetch(`${apiUrl}/balance`, {
+                    headers: { Authorization: tokenToParse },
+                }); // Replace with your API endpoint
+                const jsonBalance = await response.json();
+                await console.log(jsonBalance);
+                setBalance(jsonBalance.balance);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchBalance();
+    }, [token]);
     return (
         <main className="mx-32">
             <div className="mt-20">
-                <MoneyWrapper
-                    income="17.400,00"
-                    expense="1.259,00"
-                    balance="16.141,00"
-                />
+                {token ? (
+                    <MoneyWrapper
+                        income="17.400,00"
+                        expense="1.259,00"
+                        balance="16.141,00"
+                    />
+                ) : (
+                    <MoneyWrapper
+                        income="17.400,00"
+                        expense="1.259,00"
+                        balance="16.141,00"
+                    />
+                )}
             </div>
 
             <div className="mt-16">
-                {/* <NewTransactionModal /> */}
-                <span>asdqda</span>
-            </div>
-
-            <div>
-                {data ? (
-                    <pre>{JSON.stringify(data, null, 2)}</pre>
-                ) : (
-                    <p>Loading data...</p>
-                )}
+                <NewTransactionModal />
+                <button onClick={() => signOut()}>sair</button>
+                
             </div>
         </main>
     );
--- apps/web/src/app/api/token/route.ts
+++ apps/web/src/app/api/token/route.ts
@@ -9,5 +9,5 @@ export async function GET(req: NextRequest) {
 
     
     return NextResponse.json(token);
-
+    
 }
