3327d6b2fb75daf17b14c50610081d65ef4f7212
Gustavo Mendonça <alf4r6@gmail.com>
2023-12-17 04:21:05 -0300

    test: fuzz test to auth user

--- apps/backend/src/auth/auth.controller.spec.ts
+++ apps/backend/src/auth/auth.controller.spec.ts
@@ -1,14 +1,31 @@
 import { INestApplication } from "@nestjs/common";
-import { createNestAppInstance } from "test/test.helper";
+import { createNestAppInstance, deleteUserByEmail } from "test/test.helper";
 import request from "supertest";
 import { IAuth } from "./dtos/signin-dtos";
+import { IUser } from "@/users/dtos/create-user-dtos";
 
 const credentials: IAuth[] = [
     {
         email: "invalid@email.com",
         password: "invalidpassword",
     },
+    {
+        email: "",
+        password: "invalidpassword",
+    },
+    {
+        email: "invalidemail.com",
+        password: "",
+    },
 ];
+
+const user: IUser = {
+    firstName: "Auth",
+    lastName: "Auth",
+    email: "auth@testEmail.com",
+    password: "authPassword",
+    avatarUrl: "https://www.testavatar.com",
+};
 
 describe("AuthController", () => {
     let app: INestApplication;
@@ -23,6 +40,34 @@ describe("AuthController", () => {
                 .post("/auth")
                 .send(credentials[0])
                 .expect(400);
+        });
+
+        it("should not be able to authenticate without email", async () => {
+            await request(app.getHttpServer())
+                .post("/auth")
+                .send(credentials[1])
+                .expect(400);
+        });
+
+        it("should not be able to authenticate without password", async () => {
+            await request(app.getHttpServer())
+                .post("/auth")
+                .send(credentials[2])
+                .expect(400);
+        });
+
+        it("should be able to authenticate with valid credentials", async () => {
+            await request(app.getHttpServer()).post("/users").send(user);
+            const response = await request(app.getHttpServer())
+                .post("/auth")
+                .send({
+                    email: user.email,
+                    password: user.password,
+                });
+
+            await deleteUserByEmail(user.email);
+
+            expect(response.statusCode).toBe(201);
         });
     });
 });
