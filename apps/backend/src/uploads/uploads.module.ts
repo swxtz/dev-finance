a3aa4c8731424fd93a7e3fac472c7a06d1fc1468
Gustavo Mendonça <alf4r6@gmail.com>
2023-12-19 20:23:46 -0300

    feat: add rate limit on upload file route

--- apps/backend/src/uploads/uploads.module.ts
+++ apps/backend/src/uploads/uploads.module.ts
@@ -1,9 +1,33 @@
 import { Module } from "@nestjs/common";
 import { UploadsService } from "./uploads.service";
 import { UploadsController } from "./uploads.controller";
+import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
+import { APP_GUARD } from "@nestjs/core";
+import { ConfigService } from "@nestjs/config";
 
 @Module({
+    imports: [
+        ThrottlerModule.forRootAsync({
+            useFactory: (configService: ConfigService) => [
+                {
+                    ttl: configService.getOrThrow<number>(
+                        "UPLOAD_THROTTLE_TTL",
+                    ),
+                    limit: configService.getOrThrow<number>(
+                        "UPLOAD_THROTTLE_LIMIT",
+                    ),
+                },
+            ],
+            inject: [ConfigService],
+        }),
+    ],
     controllers: [UploadsController],
-    providers: [UploadsService],
+    providers: [
+        UploadsService,
+        {
+            provide: APP_GUARD,
+            useClass: ThrottlerGuard,
+        },
+    ],
 })
 export class UploadsModule {}
