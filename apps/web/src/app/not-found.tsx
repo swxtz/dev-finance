4adce9c5fcf482bc83bfe8f06ede99d9c0f13ed0
Gustavo Mendonça <alf4r6@gmail.com>
2024-01-24 14:53:25 -0300

    feat: creating not-found page

---
+++ apps/web/src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "404 | Dev.Finance",
+    description: "Ops... Página não encontrada",
+};
+
+export default function NotFound() {
+    return (
+        <div className="flex flex-col justify-center items-center h-screen">
+            <h1 className="text-5xl text-gray-200">404</h1>
+            <h2 className="text-3xl text-gray-200">Página não encontrada</h2>
+        </div>
+    );
+}
